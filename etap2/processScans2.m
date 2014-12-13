1f2e66b71194f8d5aab0e2b5f206142bee53ae47
gre123 <godzilla123@tlen.pl>
2014-12-13 20:08:12 +0100

    Dalej druga metoda

--- etap2/processScans2.m
+++ etap2/processScans2.m
@@ -9,9 +9,37 @@
     % image - ca³y obraz
     % name - nazwa obrazu
 
-function processed = processScansSVM(allData,range1,range2)
-   processed = preprocessingConversionAndKMeanClustering(allData,range1,range2);
-   
-   %processed = subImaging(allData,range1,range2);
-  % [texture,shape] = extractFeatures(allData,range1,range2);
-   return;
+function processed = processScans2(allData,range1,range2)
+dataSize = range2;
+if range2 == 0
+    dataSize = numel(allData);
+end 
+cform = makecform('srgb2lab');
+for i = range1:dataSize
+    currentImage = allData(i).image;
+    %Zwiekszanie kontrastu
+    allData(i).image = imadjust(currentImage);
+    %Medianka
+    allData(i).image = medfilt2(allData(i).image,[2 2]);
+    allData(i).image(:,:,2) = allData(i).image(:,:,1);
+    allData(i).image(:,:,3) = allData(i).image(:,:,1);
+    
+    %RGB -> Lab
+    allData(i).image = applycform(allData(i).image,cform);
+    figure(1)
+    imshow(allData(i).image);
+    figure(2)
+    
+    %K-mean Clustering
+    nColors = 3;
+    currentImage = double(allData(i).image(:,:,1:2));
+    ab = reshape(currentImage,1024*1024,2);
+    % repeat the clustering 3 times to avoid local minima
+    [cluster_idx] = kmeans(ab,nColors);
+    nrows = size(currentImage,1);
+    ncols = size(currentImage,2);
+    pixel_labels = reshape(cluster_idx,nrows,ncols);
+    imshow(pixel_labels,[]), title('image labeled by cluster index');
+    processed = cluster_idx;
+end
+return;
